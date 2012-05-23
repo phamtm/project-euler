8e09ff1f1ffc8ef86e2dfd5fda189ffa39b465cd
Minh Pham <phamminh91@gmail.com>
2012-05-23 22:09:24 +0700

    solved the big int problem 13

---
+++ 013.py
@@ -0,0 +1,27 @@
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+def prob13():
+	sum_str = ""
+	array = []
+	f = open("013.txt", "r")
+	for line in f:
+		array.append(line.strip())
+	f.close()
+
+	carry = 0
+	for i in range(len(array[0])-1, -1, -1):
+		temp_sum = carry
+
+		for string in array:
+			temp_sum += int(string[i])
+
+		# save for next calculation
+		carry = int((temp_sum - temp_sum %10) / 10)
+		temp_sum = temp_sum%10
+		sum_str = str(temp_sum) + sum_str
+
+	if carry != 0:
+		sum_str = str(carry) + sum_str
+	return sum_str[0:10]
+
+print(prob13())
